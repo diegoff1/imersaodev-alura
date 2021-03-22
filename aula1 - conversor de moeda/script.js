4ccb28f0359276a2392262f0cd3131e0ca5c7ee7
diegoff1 <diego.farias.fernandes@gmail.com>
2021-03-22 19:08:30 -0300

    Add files via upload
    Primeira aula do Imersão Dev - Alura. Criando um conversor de dólar para real.

---
+++ aula1 - conversor de moeda/script.js
@@ -0,0 +1,3 @@
+var dolar = parseFloat(prompt("Informe o valor em dólar(U$) para conversão."))//passando para decimais
+var real = dolar * 5.52
+alert("Valor em reais: R$ "+real.toFixed(2)) //Fixado em 2 casas após vírgula
